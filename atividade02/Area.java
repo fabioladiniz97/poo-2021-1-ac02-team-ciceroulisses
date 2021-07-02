024b0f609a1ef3fed518396d771bcba5f47c1396
Lucas <72669100+CiceroLucas@users.noreply.github.com>
2021-07-02 18:41:36 -0300

    commit Lucas

---
+++ atividade02/Area.java
@@ -0,0 +1,18 @@
+/*Crie um programa que calcule a área de um retângulo, declare e
+inicialize duas variáveis para testá-lo, imprimindo esses resultados
+na tela.*/
+
+
+public class Area {
+  public static void main(String[] args) {
+    
+    int base = 10;
+    int altura = 5;
+    int area = base * altura;
+
+    
+
+    System.out.printf("A area total é: %d", area);
+
+  }
+}
